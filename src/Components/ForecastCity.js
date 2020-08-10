bb3419992842ea6d4f4d60a1fe8994eeea9c5ed6
Dumitru Stefirta <dimastefirta@gmail.com>
2020-08-10 13:47:52 +0300

    On branch master

--- src/Components/ForecastCity.js
+++ src/Components/ForecastCity.js
@@ -7,7 +7,6 @@ export function ForecastCity(props) {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        // if (!props.selectedCity || !cities[props.selectedCity]) return
         getforecastCity(props.cityName).then((data) => {
             return data.list.reduce((list, item) => {
                 const dateKeys = new Date(item.dt_txt).getDate();
@@ -18,7 +17,6 @@ export function ForecastCity(props) {
                 return list;
             }, {})
         }).then(result => {
-            console.log(result)
             setData(result);
             setIsLoading(false);
         })
@@ -29,22 +27,18 @@ export function ForecastCity(props) {
         return (<p>Loading...</p>);
     }
     if (data) {
-        for(let day in data){
-            console.log(data[day])
-            data[day].map(item => {
-                const fixedTime = new Date(item.dt_txt).getHours();
-                if(fixedTime === 12) {
-                    return (
-                        <div className='forecast-box__daily'>
-                                {item.main.temp}
-                        </div>
+        for (let day in data) {
+            return (
+                data[day].map((item, index) => {
+                    const fixedTime = new Date(item.dt_txt).getHours();
+                    if(fixedTime) return (
+                        <div className='forecast-box__daily' key={index}>{
+                            <span>{Math.floor(item.main.temp)}</span>
+                        }</div>
                     )
-
-                }
-
-            })
+                })
+            )
         }
     }
     return (<p>Sorry could not get data, please refresh page</p>)
-
 }
